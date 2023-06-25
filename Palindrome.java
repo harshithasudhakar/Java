348c9a86fb33d009fbca9c3ef41369c56e13da63
Harshitha Sudhakar <111514477+harshithasudhakar@users.noreply.github.com>
2023-06-25 14:05:37 +0530

    Add files via upload
    Checks if the given input is a Palindrome

---
+++ Palindrome.java
@@ -0,0 +1,28 @@
+//Checks if the given input is a Palindrome
+import java.util.Scanner;
+
+class Palindrome{
+    public boolean isPalindrome(int x) {
+        int y = x;
+        int a,n = 0;
+        while(x>0){
+            a = x%10;
+            x = x/10;
+            n = (n*10)+a;
+        }
+        if(n==y){
+            return true;
+            }
+        else{
+            return false;
+            }
+    }
+
+    public static void main(String[] args){
+        Scanner input= new Scanner(System.in);
+        int x= input.nextInt();
+        Palindrome obj = new Palindrome();
+        System.out.println(obj.isPalindrome(x));
+    }
+}
+
