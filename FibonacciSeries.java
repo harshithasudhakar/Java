eac953183e0e80a935b6c1d564ced833ffa9a84f
Harshitha Sudhakar <111514477+harshithasudhakar@users.noreply.github.com>
2023-06-25 23:25:05 +0530

    Add files via upload

---
+++ FibonacciSeries.java
@@ -0,0 +1,25 @@
+//Fibonacci Series of 'n' Numbers
+
+import java.util.Scanner;
+
+class FibonacciSeries{
+	void fib(int n){
+		int t1=1;
+		int t2=1;
+		int t3=2;
+		System.out.println("The series is: 1, 1, 2...");
+		for(int i=3;i<n;i++){
+			t1=t2;
+			t2=t3;
+			t3=t2+t1;
+			System.out.println(t3);
+		}
+	}
+	
+	public static void main(String[] args){
+		Scanner input= new Scanner(System.in);
+		int n = input.nextInt();
+		FibonacciSeries obj = new FibonacciSeries();
+		obj.fib(n);
+	}
+}
