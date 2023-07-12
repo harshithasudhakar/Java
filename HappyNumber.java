67424c22ecddc845fe87b92164841317c8c7a38b
Harshitha Sudhakar <111514477+harshithasudhakar@users.noreply.github.com>
2023-07-12 18:34:17 +0530

    Update HappyNumber.java

--- HappyNumber.java
+++ HappyNumber.java
@@ -1,18 +1,19 @@
 //Happy Number
 import java.util.Scanner;
 
-class HappyNumber{
-	int x;
-    int a,n = 0;
-	public void splitDigits(int x){
-        while(x>0){
-            a = x%10;
-            x = x/10;
-            n += a*a;
-		}
-	}
-	
-	boolean isHappy(){
+class HappyNumber {
+    int n = 0;
+
+    public void splitDigits(int x) {
+        n = 0; // Reset the value of n
+        while (x > 0) {
+            int a = x % 10;
+            x = x / 10;
+            n += a * a;
+        }
+    }
+
+    boolean isHappy() {
         int slow = n;
         int fast = n;
 
@@ -22,14 +23,14 @@ class HappyNumber{
             splitDigits(fast);
         } while (slow != fast);
 
-        return slow==1;
+        return slow == 1;
     }
-	
-	public static void main(String[] args){
-		Scanner input = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
         int x = input.nextInt();
         HappyNumber obj = new HappyNumber();
-		obj.splitDigits(x);
+        obj.splitDigits(x);
         System.out.println(obj.isHappy());
-	}
+    }
 }
