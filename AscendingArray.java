5a9c96f76c50c3aab354930c87f88e7b11f93cc0
Harshitha Sudhakar <111514477+harshithasudhakar@users.noreply.github.com>
2023-07-17 15:01:16 +0530

    Update AscendingArray.java

--- AscendingArray.java
+++ AscendingArray.java
@@ -12,7 +12,7 @@ class AscendingArray {
         for (int i = 0; i < n; i++) {
             arr[i] = input.nextInt();
         }
-        System.out.println(Arrays.toString(arr));
+        System.out.println("Input array: " + Arrays.toString(arr));
     }
 
     void swap(int i, int j) {
@@ -23,16 +23,16 @@ class AscendingArray {
 
     void arrange() {
         int l = arr.length;
-        for (int i = 0; i < l; i++) {
-            int greatest = i;
+        for (int i = 0; i < l - 1; i++) {
+            int smallest = i; // Change variable name to "smallest"
             for (int j = i + 1; j < l; j++) {
-                if (arr[j] > arr[greatest]) {
-                    greatest = j;
+                if (arr[j] < arr[smallest]) { // Change the comparison to <
+                    smallest = j; // Change the assignment to "smallest"
                 }
             }
-            swap(greatest, l - 1);
-            l--;
+            swap(i, smallest); // Swap with the smallest element
         }
+        System.out.println("The greatest element in the array is: " + arr[arr.length - 1]); // The last element is the greatest
         System.out.println("The arranged array in ascending order is: " + Arrays.toString(arr));
     }
 
