21672daec18086693b8e0b195c89953c1a3c2928
Harshitha Sudhakar <111514477+harshithasudhakar@users.noreply.github.com>
2023-07-21 09:18:11 +0530

    Update PascalsTriangle.java

--- PascalsTriangle.java
+++ PascalsTriangle.java
@@ -1,37 +1,35 @@
 //Pascal's Triangle
 
 /*
-1
-1 1
-1 2 1
-1 3 3 1
-1 4 6 4 1
-1 5 10 10 5 1
- 
+        1
+      1   1
+    1   2   1
+  1   3   3   1
+1   4   6   4   1
+
 */
 
 import java.util.Scanner;
 
-class PascalsTriangle{
-	
-	void pTriangle(int n){
-		int count=1;
-		System.out.println("1");
-		System.out.println("1"+" 1");
-		for(int i=1;i<=n;i++){
-			System.out.println("1");
-			for(int j=1;j<=i;j++){
-				System.out.print();
-			}
-			System.out.println("1");
-			System.out.println("");
-		}
-	}
-		
-	public static void main(String[] args){
-		Scanner input= new Scanner(System.in);
-		int n= input.nextInt();
-		PascalsTriangle obj = new PascalsTriangle();
-		obj.pTriangle(n);
-	}
+class PascalsTriangle {
+
+    void pTriangle(int n) {
+        for (int row = 0; row < n; row++) {
+            int num = 1;
+            System.out.printf("%" + (n - row) * 2 + "s", ""); // Print leading spaces
+            for (int i = 0; i <= row; i++) {
+                System.out.printf("%4d", num);
+                num = num * (row - i) / (i + 1);
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter the number of rows for Pascal's Triangle: ");
+        int n = input.nextInt();
+        PascalsTriangle obj = new PascalsTriangle();
+        obj.pTriangle(n);
+    }
 }
