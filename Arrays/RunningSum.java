4e57d855dcab4115b8ca7c160c0f332d0bcea30b
Harshitha Sudhakar <111514477+harshithasudhakar@users.noreply.github.com>
2023-08-17 00:03:14 +0530

    Update and rename RunningSum.java to Arrays/RunningSum.java

--- Arrays/RunningSum.java
+++ Arrays/RunningSum.java
@@ -1,12 +1,12 @@
-//Running Sum of 1d Array
+//Running Sum of 1D Array
 
 import java.util.Scanner;
 
 class RunningSum{
     public static int[] runningSum(int[] nums) {
         int[] arr = new int[nums.length];
-        int sum=0;
-        for(int i=0; i<nums.length; i++){
+        int sum = 0;
+        for(int i = 0; i<nums.length; i++){
             sum+=nums[i];
             arr[i]=sum;
         }
@@ -17,7 +17,7 @@ class RunningSum{
         Scanner input = new Scanner(System.in);
         int n = input.nextInt();
         int[] nums = new int[n];
-        for(int i=0; i<n; i++){
+        for(int i = 0; i<n; i++){
             nums[i]=input.nextInt();
         }
         runningSum(nums);
