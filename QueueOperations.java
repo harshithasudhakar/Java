eaba9ccdaedbdf0eb297b01eddd137cee211498f
Harshitha Sudhakar <111514477+harshithasudhakar@users.noreply.github.com>
2023-10-16 12:48:03 +0530

    Update and rename QueuePractice.java to QueueOperations.java

--- QueueOperations.java
+++ QueueOperations.java
@@ -1,7 +1,7 @@
 import java.util.LinkedList;
 import java.util.Queue;
 
-class QueuePractice{
+class QueueOperations{
 
     public static void main(String[] args){
         Queue<Integer> queue = new LinkedList<>();
@@ -14,8 +14,9 @@ class QueuePractice{
             System.out.print(i + " ");
         }
         System.out.println(queue.remove());
+	queue.peek();
         queue.remove();
-		queue.remove();
+	queue.remove();
         for (Integer i : queue) {
             System.out.print(i + " ");
         }
