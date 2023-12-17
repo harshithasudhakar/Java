07a85c963bfc6fc837df8712f31db5ed3b70d605
Harshitha Sudhakar <111514477+harshithasudhakar@users.noreply.github.com>
2023-12-17 11:41:29 +0530

    Add files via upload

---
+++ SelectionSort.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class SelectionSort{
+    int temp;
+    void swap(int[] arr){
+        if(arr.length == 0){
+            return;
+        }
+        else{
+            System.out.println("The sorted array is: ");
+            for(int i = 0; i<arr.length; i++){
+                for(int j = i; j<arr.length; j++){
+                    if(arr[i]>arr[j]){
+                        temp = arr[i];
+                        arr[i] = arr[j];
+                        arr[j] = temp;
+                    }
+                }
+                System.out.print(arr[i] + " ");
+            }
+        }
+    }
+    public static void main(String[] args){
+        SelectionSort ss = new SelectionSort();
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter the length of array: ");
+        int n = input.nextInt();
+        int[] arr = new int[n];
+        for(int i = 0; i<n; i++){
+            arr[i] = input.nextInt();
+        }
+
+        ss.swap(arr);
+    }
+}
