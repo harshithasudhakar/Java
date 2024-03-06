ad8301db97bc3d4a27d21bdc18cb805408d01b5c
Harshitha Sudhakar <111514477+harshithasudhakar@users.noreply.github.com>
2024-03-06 11:22:49 +0530

    Create DefangingIPaddress.java

---
+++ Strings/DefangingIPaddress.java
@@ -0,0 +1,9 @@
+class Solution {
+    public static String defangIPaddr(String address) {
+        return address.replaceAll("\\.","[.]");
+    }
+    public static void main(String[] args){
+        String address = new String();
+        defangIPaddr(address);
+    }
+}
