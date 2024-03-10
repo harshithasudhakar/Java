0c119ffd2991848e02dcb23325d4d9b337af9405
Harshitha Sudhakar <111514477+harshithasudhakar@users.noreply.github.com>
2024-03-10 16:16:19 +0530

    Create MinStringLength.java

---
+++ Stacks/Problems/MinStringLength.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+import java.util.Stack;
+
+class MinStringLength {
+    public static int minLength(String s) {
+        Stack<Character> stack = new Stack<>();
+        stack.push(s.charAt(0));
+        for(int i = 1; i<s.length(); i++){
+            if(stack.peek() == 'A' && s.charAt(i) == 'B'){
+                stack.pop();
+                continue;
+            }
+            if(stack.peek() == 'C' && s.charAt(i) == 'D'){
+                stack.pop();
+                continue;
+            }
+            else{
+                stack.push(s.charAt(i));
+            }
+        }
+        return lengthOfStack(stack);
+    }
+
+    public static int lengthOfStack(Stack<Character> stack){
+        int min_length = 0;
+        while(stack.empty() == false){
+            stack.pop();
+            min_length += 1;
+        }
+        return min_length;
+    }
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        String s = input.nextLine();
+        minLength(s);
+    }
+}
