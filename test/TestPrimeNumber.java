11150221f8fb3ff8adb10fbcbfe39c32c213612a
Harshitha Sudhakar <111514477+harshithasudhakar@users.noreply.github.com>
2024-03-16 23:39:33 +0530

    fix compilation errors

--- test/TestPrimeNumber.java
+++ test/TestPrimeNumber.java
@@ -6,15 +6,15 @@ class TestPrimeNumber {
   @Test
   void testIsPrime(){
     PrimeNumber prime = new PrimeNumber();
-    assertTrue(PrimeNumber.checkIfPrime(2));
-    assertTrue(PrimeNumber.checkIfPrime(3));
-    assertTrue(PrimeNumber.checkIfPrime(7));
+    assertTrue(prime.checkIfPrime(2));
+    assertTrue(prime.checkIfPrime(3));
+    assertTrue(prime.checkIfPrime(7));
   }
 
   void testIsNotPrime(){
     PrimeNumber prime = new PrimeNumber();
-    assertTrue(PrimeNumber.checkIfPrime(4));
-    assertTrue(PrimeNumber.checkIfPrime(6));
-    assertTrue(PrimeNumber.checkIfPrime(10));
+    assertFalse(prime.checkIfPrime(4));
+    assertFalse(prime.checkIfPrime(6));
+    assertFalse(prime.checkIfPrime(10));
   }
 }
