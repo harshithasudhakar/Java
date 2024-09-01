417f595aa471c550c21c1e9ae384d7bed48cb05e
Harshitha Sudhakar <111514477+harshithasudhakar@users.noreply.github.com>
2024-09-02 00:27:52 +0530

    add ListNode

--- Linked List/Singly Linked List/FindMiddleNode.java
+++ Linked List/Singly Linked List/FindMiddleNode.java
@@ -1,4 +1,14 @@
+import java.util.Scanener;
+
 class FindMiddleNode {
+    public class ListNode {
+        int val;
+        ListNode next;
+        ListNode() {}
+        ListNode(int val) { this.val = val; }
+        ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+    }
+    
     public ListNode middleNode(ListNode head) {
         if (head == null || head.next == null) {
             return head;
