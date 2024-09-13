037217dc7329d9d01ee6fc6e0464310be7a78f24
Harshitha Sudhakar <111514477+harshithasudhakar@users.noreply.github.com>
2024-09-14 00:04:52 +0530

    add ListNode

--- Linked List/Singly Linked List/RemoveElements.java
+++ Linked List/Singly Linked List/RemoveElements.java
@@ -1,14 +1,15 @@
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
- */
+import java.util.Scanner;
+
 class RemoveElements {
+    
+    public class ListNode {
+        int val;
+        ListNode next;
+        ListNode() {}
+        ListNode(int val) { this.val = val; }
+        ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+    }
+    
     public ListNode removeElements(ListNode head, int val) {
 
         while (head != null && head.val == val) {
