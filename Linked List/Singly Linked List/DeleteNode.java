53b80a0083f7afcae5d0c7d9216d84e904d3a43f
Harshitha Sudhakar <111514477+harshithasudhakar@users.noreply.github.com>
2024-09-13 23:27:07 +0530

    Update DeleteNode.java

--- Linked List/Singly Linked List/DeleteNode.java
+++ Linked List/Singly Linked List/DeleteNode.java
@@ -1,12 +1,14 @@
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode(int x) { val = x; }
- * }
- */
+import java.util.Scanner;
+
 class DeleteNode {
+    public class ListNode {
+        int val;
+        ListNode next;
+        ListNode() {}
+        ListNode(int val) { this.val = val; }
+        ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+    }
+    
     public void deleteNode(ListNode node) {
         node.val = node.next.val;
         node.next = node.next.next;
