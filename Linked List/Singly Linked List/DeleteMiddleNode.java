2c182f8203a6c503452d45a9793a977f99fa8826
Harshitha Sudhakar <111514477+harshithasudhakar@users.noreply.github.com>
2024-09-18 22:40:18 +0530

    Update DeleteMiddleNode.java

--- Linked List/Singly Linked List/DeleteMiddleNode.java
+++ Linked List/Singly Linked List/DeleteMiddleNode.java
@@ -1,4 +1,13 @@
 class DeleteMiddleNode {
+    
+    public class ListNode {
+        int val;
+        ListNode next;
+        ListNode() {}
+        ListNode(int val) { this.val = val; }
+        ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+    }
+    
     public ListNode deleteMiddle(ListNode head) {
         if (head == null || head.next == null) {
             return null;
