a235064838e240dad0dddd666326645e7999d3f1
Harshitha Sudhakar <111514477+harshithasudhakar@users.noreply.github.com>
2024-09-23 19:52:22 +0530

    readability

--- Math/BinaryPalindrome.java
+++ Math/BinaryPalindrome.java
@@ -1,15 +1,22 @@
 import java.util.*;
+
 public class BinaryPalindrome{
+    
     public static boolean isBinaryPalindrome(int num){
+        
         int revBinary = 0;
         int copyNum = num;
+        
         while(copyNum != 0){
             revBinary = (revBinary<<1) | (copyNum & 1);
             copyNum >>= 1;
         }
+        
         return (revBinary == num);
     }
+    
     public static void main(String[] args){
+        
         Scanner input = new Scanner(System.in);
         System.out.println("Enter number: ");
         int num = input.nextInt();
