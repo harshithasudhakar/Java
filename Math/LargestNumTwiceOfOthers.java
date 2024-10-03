1b9b1f7411d877c2093ceabe74c141c7a5e3b6de
Harshitha Sudhakar <111514477+harshithasudhakar@users.noreply.github.com>
2024-10-03 15:04:54 +0530

    Create LargestNumTwiceOfOthers.java

---
+++ Math/LargestNumTwiceOfOthers.java
@@ -0,0 +1,26 @@
+class LargestNumTwiceOfOthers {
+    public static int dominantIndex(int[] nums) {
+        for(int i = 0; i < nums.length; i++){
+            int count = 0;
+            for(int j = 0; j < nums.length; j++){
+                if(i != j && nums[i] >= (2*nums[j])){
+                    count ++;
+                }
+            }
+            if(count == nums.length - 1){
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        int n = input.nextInt();
+        int[] nums = new int[n];
+        for(int i = 0; i<n; i++){
+            nums[i] = input.nextInt();
+        }
+        dominantIndex(nums);
+    }
+}
