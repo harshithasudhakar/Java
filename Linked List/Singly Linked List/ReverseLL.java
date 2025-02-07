edc3b98b6af80319d2d74e11a107d629ef6073a6
Harshitha Sudhakar <111514477+harshithasudhakar@users.noreply.github.com>
2025-02-08 00:14:02 +0530

    Update ReverseLL.java

--- Linked List/Singly Linked List/ReverseLL.java
+++ Linked List/Singly Linked List/ReverseLL.java
@@ -1,30 +1,69 @@
-/*
-public class Main {
+import java.util.*;
 
-    static class Node {
-        int data;
+class ReversingSLL{
+
+    static class Node{
+        int val;
         Node next;
-
-        public Node(int data) {
-            this.data = data;
+        Node(int val){
+            this.val = val;
             this.next = null;
         }
-    }*/
-public static Node reverseList(Node head) {
+    }
+
+    static Node head;
+
+    static void addNode(int val){
+        Node current = head;
+        Node newNode = new Node(val);
+        if(head == null){
+            head = newNode;
+            return;
+        }
+        while(current.next != null){
+            current = current.next;
+        }
+        current.next = newNode;
+    }
+
+    static Node reverse(Node head){
         Node prev = null;
         Node current = head;
-        Node next = null;
+        
+        while(current!=null){
 
-        while (current != null) {
-            next = current.next;
-
+            Node nextNode = current.next;
             current.next = prev;
-
             prev = current;
-            current = next;
+            current = nextNode;
         }
-
         return prev;
     }
-    
 
+    static void printList(Node head){
+        Node current = head;
+        while(current!=null){
+            System.out.print(current.val + "->");
+            current = current.next;
+        }
+        System.out.println("null");
+    }
+
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter the no.of nodes: ");
+        int n = input.nextInt();
+        int[] nodes = new int[n];
+        System.out.println("Enter the nodes: ");
+        for(int i = 0; i<n; i++){
+            nodes[i] = input.nextInt();
+        }
+
+        for(int i = 0; i<n; i++){
+            addNode(nodes[i]);
+        }
+
+        head = reverse(head);
+        printList(head);
+    }
+}
