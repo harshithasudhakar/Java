9c3da015d070e9632aa3df641d97e37c126ec8e4
Harshitha Sudhakar <111514477+harshithasudhakar@users.noreply.github.com>
2025-03-05 02:13:48 +0530

    update: implementation using adj matrix

--- Graphs/GraphsImplementation.java
+++ Graphs/GraphsImplementation.java
@@ -27,6 +27,23 @@ class GraphsImplementation{
         }
     }
 
+    static void addEdgeUsingAdjacencyMatrix(int[][] adjMatrix, Edge e){
+        if(((0 <= e.src) && (e.src < adjMatrix.length))&& ((0 <= e.dest) && (e.dest < adjMatrix.length))){
+            adjMatrix[e.src][e.dest] = 1;
+            adjMatrix[e.dest][e.src] = 1; //Undirected graph
+        }
+        else{
+            System.out.println("Node out of graph bounds, enter a valid source.");
+            return;
+        }
+        for(int i =0; i<adjMatrix.length; i++){
+            for(int j = 0; j<adjMatrix.length; j++){
+                System.out.print(adjMatrix[i][j]);
+            }
+            System.out.println();
+        }
+    }
+
     public static void main(String[] args){
         Scanner input = new Scanner(System.in);
         System.out.println("Enter the no.of vertices: ");
@@ -34,13 +51,16 @@ class GraphsImplementation{
         System.out.println("Enter the no.of edges: ");
         int e =  input.nextInt();
 
-        ArrayList<ArrayList> graph = new ArrayList<ArrayList>();
+        //ArrayList<ArrayList> graph = new ArrayList<ArrayList>();
 
-        initializeGraph(graph, v);
+        //initializeGraph(graph, v);
+
+        int[][] adjMatrix = new int[v][v];
 
         for(int i = 0; i<e; i++){
             System.out.println("Enter the edge (src, dest): ");
-            addEdge(graph, new Edge(input.nextInt(), input.nextInt()));
+            //addEdge(graph, new Edge(input.nextInt(), input.nextInt()));
+            addEdgeUsingAdjacencyMatrix(adjMatrix, new Edge(input.nextInt(), input.nextInt()));
         }
 
     }
