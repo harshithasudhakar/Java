5f97233292478e68f517d047ab65645346ced5db
Harshitha Sudhakar <111514477+harshithasudhakar@users.noreply.github.com>
2025-03-06 23:51:31 +0530

    memoization and tabulation approaches

---
+++ Dynamic Programming/FibonacciSeries.java
@@ -0,0 +1,69 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.util.*;
+import java.lang.*;
+import java.math.*;
+import java.io.*;
+
+class GFG {
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+        while (T-- > 0) {
+            int n = sc.nextInt();
+            Solution obj = new Solution();
+            long topDownans = obj.topDown(n);
+            long bottomUpans = obj.bottomUp(n);
+            if (topDownans != bottomUpans)
+                System.out.println(-1);
+            else
+                System.out.println(topDownans);
+        
+System.out.println("~");
+}
+    }
+}
+
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class FibonacciSeries { // memoization and tabulation approaches
+    
+    static final int MOD = 1000000007;
+    
+    static long topDown(int n) {
+        long[] dp = new long[n+1];
+        
+        Arrays.fill(dp, -1);
+        
+        return memoization(dp, n);
+    }
+    
+    static long memoization(long[] dp, int n){
+        if(n<=1){
+            return n;
+        }
+        
+        if(dp[n] != -1){
+            return dp[n];
+        }
+        
+        return dp[n] = (memoization(dp, n-1) + memoization(dp, n-2)) % MOD;
+    }
+
+    static long bottomUp(int n) {
+        if (n == 0) return 0;
+        if (n == 1) return 1;
+        
+        long[] dp = new long[n+1];
+        dp[0] = 0;
+        dp[1] = 1;
+        for(int i = 2; i<=n; i++){
+            dp[i] = (dp[i-1] + dp[i-2]) % MOD;
+        }
+        return dp[n];
+    }
+}
