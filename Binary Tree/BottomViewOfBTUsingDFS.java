df255cbb81e6f8228102c13125c5ed3170a621ee
Harshitha Sudhakar <111514477+harshithasudhakar@users.noreply.github.com>
2025-03-24 23:33:32 +0530

    Create BottomViewOfBTUsingDFS.java

---
+++ Binary Tree/BottomViewOfBTUsingDFS.java
@@ -0,0 +1,82 @@
+import java.util.Scanner;
+import java.util.Queue;
+import java.util.LinkedList;
+import java.util.Map;
+import java.util.TreeMap;
+
+class BottomViewOfBTUsingDFS{
+
+    static class Node{
+        int val;
+        Node right;
+        Node left;
+        Node(int val){
+            this.val = val;
+            this.left = null;
+            this.right = null;
+        }
+    }
+
+    static Node root;
+    static Queue<Node> q = new LinkedList<>();
+
+    static void buildTree(int[] nodes){
+        if(nodes.length == 0) return;
+        root = new Node(nodes[0]);
+        q.add(root);
+        int i = 1;
+
+        while(!q.isEmpty()){
+            Node current = q.poll();
+            if(i<nodes.length && nodes[i] != -1){
+                if(current.left == null){
+                    current.left = new Node(nodes[i++]);
+                    q.add(current.left);
+                }
+            }
+
+            if(i<nodes.length && nodes[i] != -1){
+                if(current.right == null){
+                    current.right = new Node(nodes[i++]);
+                    q.add(current.right);
+                }
+            }
+        }
+    }
+
+    static void bottomView(Node root){
+        Map<Integer, int[]> map = new TreeMap<>(); // {hd: [node.val, level]}
+        dfs(root, map, 0, 0);
+        System.out.print("Top view: ");
+        for(int[] pairs : map.values()){
+            System.out.print(pairs[0] + " ");
+        }
+    }
+
+    static void dfs(Node root, Map<Integer, int[]> map, int hd, int level){
+        if(root == null){
+            return;
+        }
+
+        if(!map.containsKey(hd) || map.get(hd)[1] <= level){ // we are checking the level value at the same hd in the map. 
+                                                            //if the level value we are processing is >= than the one already in map, we will put in the map
+            map.put(hd, new int[]{root.val, l});
+        }
+
+        dfs(root.left, map, hd-1, level+1);
+        dfs(root.right, map, hd+1, level+1);
+    }
+
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter the number of nodes: ");
+        int n = input.nextInt();
+        int[] nodes = new int[n];
+        System.out.println("Enter the nodes: ");
+        for(int i = 0; i<n; i++){
+            nodes[i] = input.nextInt();
+        }
+        buildTree(nodes);
+        bottomView(root);
+    }
+}
